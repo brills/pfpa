94280857bf052ad6e0e699be5f0c346fbc43b161
Brills Peng <brillsp@gmail.com>
2012-12-13 17:52:53 -0500

    added code for demo (project/demo.c)

---
+++ project/demo.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+struct list { struct list* next; int field1; char field2; int data[128]; };
+
+void generate_list1( struct list ** p, int l) {
+	struct list *ptr;
+	if (l <= 0) return;
+	*p = malloc(sizeof(struct list));
+	ptr = *p;
+	for (--l; l >= 0; --l) {
+		ptr->next = malloc(sizeof(struct list));
+		ptr = ptr->next;
+	}
+}
+
+void generate_list2( struct list ** p, int l) {
+	struct list *ptr;
+	if (l <= 0) return;
+	*p = malloc(sizeof(struct list));
+	ptr = *p;
+	for (--l; l >= 0; --l) {
+		ptr->next = malloc(sizeof(struct list));
+		ptr = ptr->next;
+	}
+	ptr->next = NULL;
+}
+
+
+int main() {
+	struct list *l1, *p1, *l2, *p2; int i; 
+
+	// generate two linked lists
+	generate_list1(&l1, 128);
+	generate_list2(&l2, 128);
+
+	// different visiting pattern: we expect two different data layout
+	// of the same type "struct list"
+	//
+	for (i = 0, p1 = l1; p1->next != NULL; p1 = p1->next) 
+		p1->field1 = i;
+
+	for (i = 0, p2 = l2; p2->next != NULL; p2 = p2->next)
+		p2->field2 = i;
+
+  return 0;
+}
